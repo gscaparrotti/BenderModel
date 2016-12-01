b4833672a14f4b4f343399f48b83e130da95ea82
giacomo <giacomo.scaparrotti@studio.unibo.it>
2016-12-01 18:52:02 +0100

    added thread safety to Restaurant

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import com.google.common.base.Preconditions;
+import org.danilopianini.concurrency.FastReadWriteLock;
 
 /**
  *
@@ -14,21 +16,34 @@ public class Restaurant implements IRestaurant {
      */
     private static final long serialVersionUID = 6813103235280390095L;
     private final Map<Integer, Map<IDish, Pair<Integer, Integer>>> tables = new HashMap<Integer, Map<IDish, Pair<Integer, Integer>>>();
-    private int tablesAmount;
+    private transient FastReadWriteLock tablesAmountLock = new FastReadWriteLock();
+    private transient FastReadWriteLock mapLock = new FastReadWriteLock();
+    private int tablesAmount = 0;
     private static final String ERROR_MESSAGE = "Dati inseriti non corretti. Controllare.";
+    
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        tablesAmountLock = new FastReadWriteLock();
+        mapLock = new FastReadWriteLock();
+    }
 
     @Override
     public int addTable() {
+        tablesAmountLock.write();
         this.tablesAmount++;
+        tablesAmountLock.release();
         return this.tablesAmount;
     }
 
     @Override
     public int removeTable() {
+        tablesAmountLock.write();
         if (tablesAmount > 0 && !tables.containsKey(tablesAmount)) {
             tablesAmount--;
+            tablesAmountLock.release();
             return tablesAmount;
         } else {
+            tablesAmountLock.release();
             throw new IllegalStateException("Il tavolo ha ancora piatti da servire");
         }
     }
@@ -40,6 +55,7 @@ public class Restaurant implements IRestaurant {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         final Map<IDish, Pair<Integer, Integer>> temp;
+        mapLock.write();
         if (tables.containsKey(table)) {
             temp = tables.get(table);
         } else {
@@ -51,6 +67,7 @@ public class Restaurant implements IRestaurant {
             temp.put(item, new Pair<Integer, Integer>(quantity, 0));
         }
         tables.put(table, temp);
+        mapLock.release();
     }
 
     @Override
@@ -60,6 +77,7 @@ public class Restaurant implements IRestaurant {
                 || tables.get(table).get(item).getX() - quantity < 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
+        mapLock.write();
         if (tables.get(table).get(item).getX() - quantity == 0) {
             tables.get(table).remove(item);
         } else {
@@ -71,6 +89,7 @@ public class Restaurant implements IRestaurant {
         if (tables.get(table).isEmpty()) {
             resetTable(table);
         }
+        mapLock.release();
     }
 
     @Override
@@ -79,7 +98,9 @@ public class Restaurant implements IRestaurant {
         if (!checkIfCorrect(table, item, 1) || !tables.containsKey(table) || tables.get(table).get(item) == null) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
+        mapLock.write();
         tables.get(table).get(item).setY(tables.get(table).get(item).getX());
+        mapLock.release();
     }
 
     @Override
@@ -87,7 +108,9 @@ public class Restaurant implements IRestaurant {
         if (table <= 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
+        mapLock.write();
         tables.remove(table);
+        mapLock.release();
     }
 
     /*
@@ -100,14 +123,22 @@ public class Restaurant implements IRestaurant {
 
     @Override
     public int getTablesAmount() {
-        return this.tablesAmount;
+        int temp;
+        tablesAmountLock.read();
+        temp = this.tablesAmount;
+        tablesAmountLock.release();
+        return temp;
     }
 
     @Override
     public Map<IDish, Pair<Integer, Integer>> getOrders(final int table) {
+        mapLock.read();
         if (table > 0 && tables.containsKey(table)) {
-            return tables.get(table);
+            final Map<IDish, Pair<Integer, Integer>> temp = tables.get(table);
+            mapLock.release();
+            return temp;
         } else {
+            mapLock.release();
             return new HashMap<IDish, Pair<Integer, Integer>>();
         }
     }

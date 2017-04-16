fbb2ca4f639b4ba5e7070ee1c980200e2946d4f3
giacomo <giacomo.scaparrotti@studio.unibo.it>
2017-04-16 11:37:07 +0200

    removed useless complication in concurrency management

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -3,9 +3,6 @@ package model;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 import com.google.common.base.Preconditions;
 import concurrency.FastReadWriteLock;
 
@@ -20,14 +17,12 @@ public class Restaurant implements IRestaurant {
     private static final long serialVersionUID = 6813103235280390095L;
     private final Map<Integer, Map<IDish, Pair<Integer, Integer>>> tables = new HashMap<Integer, Map<IDish, Pair<Integer, Integer>>>();
     private transient FastReadWriteLock tablesAmountLock = new FastReadWriteLock();
-    private transient Lock mapLock = new ReentrantLock(true);
     private int tablesAmount;
     private static final String ERROR_MESSAGE = "Dati inseriti non corretti. Controllare.";
     
     private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException { //NOPMD
         in.defaultReadObject();
         tablesAmountLock = new FastReadWriteLock();
-        mapLock = new ReentrantLock(true);
     }
 
     @Override
@@ -39,7 +34,7 @@ public class Restaurant implements IRestaurant {
     }
 
     @Override
-    public int removeTable() {
+    public synchronized int removeTable() {
         tablesAmountLock.write();
         if (tablesAmount > 0 && !tables.containsKey(tablesAmount)) {
             tablesAmount--;
@@ -52,13 +47,12 @@ public class Restaurant implements IRestaurant {
     }
 
     @Override
-    public void addOrder(final int table, final IDish item, final int quantity) {
+    public synchronized void addOrder(final int table, final IDish item, final int quantity) {
         Preconditions.checkNotNull(item);
         if (!checkIfCorrect(table, item, quantity)) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         final Map<IDish, Pair<Integer, Integer>> temp;
-        mapLock.lock();
         if (tables.containsKey(table)) {
             temp = tables.get(table);
         } else {
@@ -70,16 +64,13 @@ public class Restaurant implements IRestaurant {
             temp.put(item, new Pair<Integer, Integer>(quantity, 0));
         }
         tables.put(table, temp);
-        mapLock.unlock();
     }
 
     @Override
-    public void removeOrder(final int table, final IDish item, final int quantity) {
+    public synchronized void removeOrder(final int table, final IDish item, final int quantity) {
         Preconditions.checkNotNull(item);
-        mapLock.lock();
         if (!checkIfCorrect(table, item, quantity) || !tables.containsKey(table) || !tables.get(table).containsKey(item)
                 || tables.get(table).get(item).getX() - quantity < 0) {
-            mapLock.unlock();
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         if (tables.get(table).get(item).getX() - quantity == 0) {
@@ -93,29 +84,23 @@ public class Restaurant implements IRestaurant {
         if (tables.get(table).isEmpty()) {
             resetTable(table);
         }
-        mapLock.unlock();
     }
 
     @Override
-    public void setOrderAsProcessed(final int table, final IDish item) {
+    public synchronized void setOrderAsProcessed(final int table, final IDish item) {
         Preconditions.checkNotNull(item);
-        mapLock.lock();
         if (!checkIfCorrect(table, item, 1) || !tables.containsKey(table) || tables.get(table).get(item) == null) {
-            mapLock.unlock();
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
         tables.get(table).get(item).setY(tables.get(table).get(item).getX());
-        mapLock.unlock();
     }
 
     @Override
-    public void resetTable(final int table) {
+    public synchronized void resetTable(final int table) {
         if (table <= 0) {
             throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-        mapLock.lock();
         tables.remove(table);
-        mapLock.unlock();
     }
 
     /*
@@ -136,14 +121,10 @@ public class Restaurant implements IRestaurant {
     }
 
     @Override
-    public Map<IDish, Pair<Integer, Integer>> getOrders(final int table) {
-        mapLock.lock();
+    public synchronized  Map<IDish, Pair<Integer, Integer>> getOrders(final int table) {
         if (table > 0 && tables.containsKey(table)) {
-            final Map<IDish, Pair<Integer, Integer>> temp = tables.get(table);
-            mapLock.unlock();
-            return temp;
+            return tables.get(table);
         } else {
-            mapLock.unlock();
             return new HashMap<IDish, Pair<Integer, Integer>>();
         }
     }

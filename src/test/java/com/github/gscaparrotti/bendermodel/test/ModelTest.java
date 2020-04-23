d3ecf50ee8bfb9166b03975146d4cfbd995c6f8a
gscaparrotti <gscaparrotti@gmail.com>
2020-04-23 13:51:23 +0200

    refactor test package names

--- src/test/java/com/github/gscaparrotti/bendermodel/test/ModelTest.java
+++ src/test/java/com/github/gscaparrotti/bendermodel/test/ModelTest.java
@@ -1,4 +1,4 @@
-package test;
+package com.github.gscaparrotti.bendermodel.test;
 import static org.junit.Assert.*;
 
 import java.util.HashSet;
@@ -10,9 +10,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import com.github.gscaparrotti.bendermodel.model.Dish;
+import com.github.gscaparrotti.bendermodel.model.IRestaurant;
+import com.github.gscaparrotti.bendermodel.model.Restaurant;
 import org.junit.Test;
-
-import model.*;
 
 public class ModelTest {
 
